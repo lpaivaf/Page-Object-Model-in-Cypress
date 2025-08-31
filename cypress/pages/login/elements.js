0621fa73ff87bfb80286b2f8a278cbbd38f524f0
Leandro Paiva <159908364+lpaivaf@users.noreply.github.com>
2025-08-31 12:15:39 +0100

    Atualizar o elements.js

--- cypress/pages/login/elements.js
+++ cypress/pages/login/elements.js
@@ -1,6 +1,6 @@
 //Arquivo de elementos mapeados para interaçãoes da página login
 
-export const elements = {  //Arrei contendo objetos já sendo exportados
+export const elements = {  //Objetos já sendo exportados
     username: '[data-test="username"]', // objetos contendo o caminho dos elementos
     password: '[data-test="password"]',
     loginButton: '#login-button',
